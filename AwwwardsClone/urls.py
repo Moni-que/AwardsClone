c9f7d670a71e575f68ef20992f069daf6fa46495
moni-que <monicahjustus@gmail.com>
2022-06-14 18:06:09 +0300

    add a search url pattern

--- AwwwardsClone/urls.py
+++ AwwwardsClone/urls.py
@@ -9,7 +9,8 @@ urlpatterns = [
     re_path(r'^signup',views.signup,name='signup'),
     re_path(r'^$',views.signin,name='signin'),
     re_path(r'^logout',views.logout,name='logout'),
-    re_path(r'^upload', views.upload, name = "upload"),
+    re_path(r'^upload',views.upload,name = "upload"),
+    re_path(r'^search/',views.search_results,name='search_results'),
 ]
 
 
