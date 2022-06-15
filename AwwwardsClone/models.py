564622d0b61ccc1cdd4c90d9fd91a6fc03b954a3
moni-que <monicahjustus@gmail.com>
2022-06-15 15:00:08 +0300

    change upload to

--- AwwwardsClone/models.py
+++ AwwwardsClone/models.py
@@ -4,7 +4,7 @@ from django.contrib.auth.models import User
 # Create your models here.
 class Profile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
-    profile_photo = models.ImageField(upload_to='images')
+    profile_photo = models.ImageField(upload_to='profile')
     bio = models.TextField(max_length=500, blank=True, null=True)
     contact = models.CharField(max_length=50, blank=True, null=True)
 
